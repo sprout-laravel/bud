84e055ba770128eb4514f5020d1045907591104c
Ollie Read <code@ollieread.com>
2025-05-01 13:07:36 +0100

    chore: Add a base creator class for simplifying retrieving the config from a bud store

---
+++ src/Overrides/BaseCreator.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace Sprout\Bud\Overrides;
+
+use RuntimeException;
+use Sprout\Bud\Bud;
+use Sprout\Exceptions\TenancyMissingException;
+use Sprout\Exceptions\TenantMissingException;
+use Sprout\Sprout;
+
+abstract class BaseCreator
+{
+    /**
+     * @param \Sprout\Sprout                                    $sprout
+     * @param \Sprout\Bud\Bud                                   $bud
+     * @param array<string, mixed>&array{budStore?:string|null} $config
+     * @param string                                            $name
+     *
+     * @return array<string, mixed>
+     *
+     * @throws \Sprout\Exceptions\MisconfigurationException
+     * @throws \Sprout\Exceptions\TenancyMissingException
+     * @throws \Sprout\Exceptions\TenantMissingException
+     */
+    public function getConfig(Sprout $sprout, Bud $bud, array $config, string $name): array
+    {
+        // If we're not within a multitenanted context, we need to error
+        // out, as this driver shouldn't be hit without one
+        if (! $sprout->withinContext()) {
+            throw TenancyMissingException::make();
+        }
+
+        // Get the current active tenancy
+        $tenancy = $sprout->getCurrentTenancy();
+
+        // If there isn't one, that's an issue as we need a tenancy
+        if ($tenancy === null) {
+            throw TenancyMissingException::make();
+        }
+
+        // If there is a tenancy, but it doesn't have a tenant, that's also
+        // an issue
+        if ($tenancy->check() === false) {
+            throw TenantMissingException::make($tenancy->getName());
+        }
+
+        /** @var \Sprout\Contracts\Tenant $tenant */
+        $tenant = $tenancy->tenant();
+
+        // Get the default store, or the one specified in the config, if there
+        // is one.
+        $store = $bud->store($this->config['budStore'] ?? null);
+
+        // Get the config for the connection from the store.
+        $budConfig = $store->get(
+            $tenancy,
+            $tenant,
+            'database',
+            $name,
+        );
+
+        // If there isn't any config, it's an error.
+        if ($budConfig === null) {
+            // TODO: Throw a better exception
+            throw new RuntimeException(sprintf(
+                'Unable to find database configuration for connection [%s] for tenant [%s] on tenancy [%s]',
+                $name,
+                $tenant->getTenantIdentifier(),
+                $tenancy->getName()
+            ));
+        }
+
+        return array_merge($config, $budConfig);
+    }
+}
--- src/Overrides/Database/BudDatabaseConnectionCreator.php
+++ src/Overrides/Database/BudDatabaseConnectionCreator.php
@@ -7,6 +7,7 @@ use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\DatabaseManager;
 use RuntimeException;
 use Sprout\Bud\Bud;
+use Sprout\Bud\Overrides\BaseCreator;
 use Sprout\Exceptions\TenancyMissingException;
 use Sprout\Exceptions\TenantMissingException;
 use Sprout\Sprout;
@@ -15,9 +16,9 @@ use Sprout\Sprout;
  * Bud Database Connection Creator
  *
  * This class is an abstraction for the logic that creates a database connection
- * using config store within Bud.
+ * using a config store within Bud.
  */
-final class BudDatabaseConnectionCreator
+final class BudDatabaseConnectionCreator extends BaseCreator
 {
     private DatabaseManager $manager;
 
@@ -65,58 +66,22 @@ final class BudDatabaseConnectionCreator
      */
     public function __invoke(): ConnectionInterface
     {
-        // If we're not within a multitenanted context, we need to error
-        // out, as this driver shouldn't be hit without one
-        if (! $this->sprout->withinContext()) {
-            throw TenancyMissingException::make();
-        }
+        $config = $this->getConfig($this->sprout, $this->bud, $this->config, $this->name);
 
-        // Get the current active tenancy
-        $tenancy = $this->sprout->getCurrentTenancy();
-
-        // If there isn't one, that's an issue as we need a tenancy
-        if ($tenancy === null) {
-            throw TenancyMissingException::make();
-        }
-
-        // If there is a tenancy, but it doesn't have a tenant, that's also
-        // an issue
-        if ($tenancy->check() === false) {
-            throw TenantMissingException::make($tenancy->getName());
-        }
-
-        /** @var \Sprout\Contracts\Tenant $tenant */
-        $tenant = $tenancy->tenant();
-
-        // Get the default store, or the one specified in the config, if there
-        // is one.
-        $store  = $this->bud->store($this->config['budStore'] ?? null);
-
-        // Get the config for the connection from the store.
-        $config = $store->get(
-            $tenancy,
-            $tenant,
-            'database',
-            $this->name,
-        );
-
-        // If there isn't any config, it's an error.
-        if ($config === null) {
-            // TODO: Throw a better exception
+        // We need to make sure that this is going to recurse infinitely.
+        if (isset($config['driver']) && $config['driver'] === 'bud') {
             throw new RuntimeException(sprintf(
-                'Unable to find database configuration for connection [%s] for tenant [%s] on tenancy [%s]',
-                $this->name,
-                $tenant->getTenantIdentifier(),
-                $tenancy->getName()
+                'Attempt to create cyclic bud database connection [%s] detected',
+                $this->name
             ));
         }
 
-        // If we're here, it all worked, so we'll create a dynamic connection.
+        // If we're here, it's not cyclic, so we'll create a dynamic connection.
         // We're intentionally not using the methods for creating a dynamic
         // connection because it does funky stuff with the names.
         return $this->manager->connectUsing(
             $this->name,
-            array_merge($this->config, $config),
+            $config,
             true // This is important, it needs to be here to avoid side-effect errors.
         );
     }
